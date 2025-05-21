4314e2b95d83a86e70daba8012589400ee797db8
Sakshi-BK <sakshibk90@gmail.com>
2025-05-21 21:45:02 +0530

    Add files via upload

---
+++ 4b.Swing/CountryButton.java
@@ -0,0 +1,29 @@
+package Swing2;
+import javax.swing.*;
+import java.awt.event.*;
+public class CountryButton {
+	public static void main(String[] args) {
+        JFrame frame = new JFrame("Country Button");
+        JButton btnIndia = new JButton("India");
+        JButton btnSrilanka = new JButton("Srilanka");
+        JLabel label = new JLabel("", SwingConstants.CENTER);
+
+        btnIndia.setBounds(50, 50, 100, 40);
+        btnSrilanka.setBounds(200, 50, 100, 40);
+        label.setBounds(50, 120, 250, 40);
+
+        frame.setLayout(null);
+        frame.add(btnIndia);
+        frame.add(btnSrilanka);
+        frame.add(label);
+
+        btnIndia.addActionListener(e -> label.setText("India is pressed"));
+        btnSrilanka.addActionListener(e -> label.setText("Srilanka is pressed"));
+
+        frame.setSize(400, 250);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
+
+
