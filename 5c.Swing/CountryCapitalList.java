b91659e54321a25a60caafca566080e129279c44
Sakshi-BK <sakshibk90@gmail.com>
2025-05-22 23:36:05 +0530

    Update CountryCapitalList.java

--- 5c.Swing/CountryCapitalList.java
+++ 5c.Swing/CountryCapitalList.java
@@ -1,5 +1,6 @@
-/*5b. Develop a Swing program in Java to create a Tabbed Pan of RED, BLUE and GREEN and
-display the concerned color whenever the specific tab is selected in the Pan.*/
+/*5c. Develop a Swing program in Java to add the countries USA, India, Vietnam, Canada,
+Denmark, France, Great Britain, Japan, Africa, Greenland, Singapore into a JList and
+display the capital of the countries on console whenever the countries are selected on the list.*/
 package Swing6;
 
 
